09696b1b200655532d4f9a05660e7b8a75261eae
MrDAH34 <anhhao22082004@gmail.com>
2025-06-12 22:43:53 +0700

    change cors

--- api-server/index.js
+++ api-server/index.js
@@ -15,10 +15,7 @@ const uploadRoute = require('./routes/uploadImgRoute');
 const stickerRoute = require('./routes/stickerRoute');
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'https://chat-brevdeisw-haodas-projects.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));

86f06d87e523ccca502e61d5473ada50c1a0816f
MrDAH34 <anhhao22082004@gmail.com>
2025-11-13 20:28:30 +0700

    feat: change docker compose file

--- api-server/config/db.js
+++ api-server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env.secret') });
 
 const DB_URL = process.env.DB_URL;
 
